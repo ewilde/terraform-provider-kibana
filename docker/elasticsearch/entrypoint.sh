7001b1f8ce96bc0dcef337756ae53dff798acde4
Jean-François Roche <jfroche@affinitic.be>
2019-09-18 22:01:24 +0200

    ci: manage elasticsearch users in docker
    and add test for xpack security support

--- docker/elasticsearch/entrypoint.sh
+++ docker/elasticsearch/entrypoint.sh
@@ -29,12 +29,27 @@ while IFS='=' read -r envvar_key envvar_value; do
 	# words, e.g. `cluster.name`, except for `processors` which we handle
 	# specially
 	if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ || "$envvar_key" == "processors" ]]; then
-		if [[ ! -z $envvar_value ]]; then
+		if [[ -n $envvar_value ]]; then
 			es_opt="-E${envvar_key}=${envvar_value}"
 			es_opts+=("${es_opt}")
 		fi
 	fi
 done < <(env)
+
+if [[ -f /usr/share/elasticsearch/bin/elasticsearch-users ]]; then
+	# Check for the ELASTIC_PASSWORD environment variable to set the
+	# bootstrap password for Security.
+	#
+	# This is only required for the first node in a cluster with Security
+	# enabled, but we have no way of knowing which node we are yet. We'll just
+	# honor the variable if it's present.
+	if [[ -n "$ELASTIC_PASSWORD" ]]; then
+		[[ -f /usr/share/elasticsearch/config/elasticsearch.keystore ]] || (run_as_other_user_if_needed elasticsearch-keystore create)
+		if ! (run_as_other_user_if_needed elasticsearch-keystore list | grep -q '^bootstrap.password$'); then
+			(run_as_other_user_if_needed echo "$ELASTIC_PASSWORD" | elasticsearch-keystore add -x 'bootstrap.password')
+		fi
+	fi
+fi
 
 exec /scripts/makelogs.sh &
 run_as_other_user_if_needed "$@" "${es_opts[@]}"
